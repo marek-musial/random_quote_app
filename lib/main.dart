72ed04200f128f7e7d089b36c2928b29fdb43cfd
MarekMusial <marekmusial07@gmail.com>
2024-03-18 18:45:18 +0100

    Separate home_page widget from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_quote_app/features/home/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,32 +16,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomePage(title: 'Random quote app'),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Reroll',
-        child: const Icon(Icons.refresh),
-      ),
     );
   }
 }
