f9f9b4edcb98fa6da230363672e5877f9d42c046
MarekMusial <marekmusial07@gmail.com>
2024-03-22 23:47:09 +0100

    Add second image remote data source

--- lib/data/remote_data_sources/image_remote_data_source.dart
+++ lib/data/remote_data_sources/image_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:random_quote_app/domain/models/image_model.dart';
 
@@ -20,6 +22,28 @@ class FirstRemoteDataSource implements ImageDataSource {
     final imageModel = ImageModel(
       imageUrl: json[0],
       author: null,
+    );
+    return imageModel;
+  }
+}
+
+class SecondRemoteDataSource implements ImageDataSource {
+  @override
+  Future<ImageModel?> getImageData() async {
+    final randomID = Random().nextInt(1084 + 1);
+
+    final response = await Dio().get<Map<String, dynamic>?>(
+      'https://picsum.photos/id/$randomID/info',
+    );
+    final json = response.data;
+
+    if (json == null) {
+      return null;
+    }
+
+    final imageModel = ImageModel(
+      imageUrl: 'https://picsum.photos/id/$randomID/1080',
+      author: json['author'],
     );
     return imageModel;
   }
