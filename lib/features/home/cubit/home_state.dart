13e09e078a4c45687a7417260c552af911f6a67a
MarekMusial <marekmusial07@gmail.com>
2024-05-13 22:37:04 +0200

    Add HomeState copyWith method
    The method allows to update individual state properties.

--- lib/features/home/cubit/home_state.dart
+++ lib/features/home/cubit/home_state.dart
@@ -11,4 +11,18 @@ class HomeState {
   final String? errorMessage;
   final ImageModel? imageModel;
   final QuoteModel? quoteModel;
+
+  HomeState copyWith({
+    Status? status,
+    String? errorMessage,
+    ImageModel? imageModel,
+    QuoteModel? quoteModel,
+  }) {
+    return HomeState(
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+      imageModel: imageModel ?? this.imageModel,
+      quoteModel: quoteModel ?? this.quoteModel,
+    );
+  }
 }
