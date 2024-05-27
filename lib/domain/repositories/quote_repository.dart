6b665afff1ca348a1d2fd54e2b47884d67820848
MarekMusial <marekmusial07@gmail.com>
2024-05-28 01:29:06 +0200

    Add QuotableQuoteRemoteDataSource

--- lib/domain/repositories/quote_repository.dart
+++ lib/domain/repositories/quote_repository.dart
@@ -11,6 +11,7 @@ class QuoteRepository {
     AffirmationsQuoteRemoteDataSource(),
     // BoredApiQuoteRemoteDataSource(),
     AdviceQuoteRemoteDataSource(),
+    QuotableQuoteRemoteDataSource(),
   ];
 
   Future<QuoteModel?> getQuoteModel() async {
