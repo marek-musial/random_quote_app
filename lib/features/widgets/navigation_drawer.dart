dc3a80d13096243cac5ff833f5f78fda9c4c6ccb
MarekMusial <marekmusial07@gmail.com>
2024-07-03 23:40:35 +0200

    Update AppBarDrawer layout and color
    Added LinearGradient as background, to fit with the rest of the app.
    Leading icon changes from a button used to close the drawer in
    portrait orientation, to a simple menu icon when AppBarDrawer
    shouldn't be closeable in landscape orientation.

--- lib/features/widgets/navigation_drawer.dart
+++ lib/features/widgets/navigation_drawer.dart
@@ -10,11 +10,25 @@ class AppBarDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      width: screenWidth / 5,
-      child: NavigationRail(
-        onDestinationSelected: (int index) {
-          if (Navigator.canPop(context)) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Theme.of(context).colorScheme.inversePrimary,
+            Theme.of(context).colorScheme.primary,
+          ],
+          stops: const [.75, 1],
+        ),
+      ),
+      child: Drawer(
+        backgroundColor: Colors.transparent,
+        width: screenWidth / 5,
+        shape: Border.all(style: BorderStyle.none),
+        child: NavigationRail(
+          backgroundColor: Colors.transparent,
+          onDestinationSelected: (int index) {
             Navigator.pushReplacementNamed(
               context,
               switch (index) {
@@ -23,31 +37,36 @@ class AppBarDrawer extends StatelessWidget {
                 int() => '/',
               },
             );
-          }
-        },
-        selectedIndex: index ?? 0,
-        labelType: NavigationRailLabelType.all,
-        leading: IconButton(
-          icon: const Icon(Icons.close),
-          onPressed: Navigator.of(context).pop,
+          },
+          selectedIndex: index ?? 0,
+          labelType: NavigationRailLabelType.all,
+          leading: MediaQuery.of(context).orientation == Orientation.portrait
+              ? IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: Navigator.canPop(context) ? Navigator.of(context).pop : null,
+                )
+              : SizedBox.square(
+                  dimension: screenWidth / 12,
+                  child: const Icon(Icons.menu),
+                ),
+          destinations: <NavigationRailDestination>[
+            NavigationRailDestination(
+              icon: const Icon(Icons.home),
+              label: const Text('Home'),
+              disabled: index == 0,
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.info),
+              label: const Text('About'),
+              disabled: index == 1,
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.settings),
+              label: const Text('Settings'),
+              disabled: index == 2,
+            ),
+          ],
         ),
-        destinations: <NavigationRailDestination>[
-          NavigationRailDestination(
-            icon: const Icon(Icons.home),
-            label: const Text('Home'),
-            disabled: index == 0,
-          ),
-          NavigationRailDestination(
-            icon: const Icon(Icons.info),
-            label: const Text('About'),
-            disabled: index == 1,
-          ),
-          NavigationRailDestination(
-            icon: const Icon(Icons.settings),
-            label: const Text('Settings'),
-            disabled: index == 2,
-          ),
-        ],
       ),
     );
   }
