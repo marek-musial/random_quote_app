1f2514a3186f16b946f5b1319017726eb3a6262d
MarekMusial <marekmusial07@gmail.com>
2024-07-05 21:42:40 +0200

    Update app.dart with SettingsPage
    Added SettingsPage route to navigation routes.
    Updated theme brightness to be dependent on RootState.
    Change the BlocProvider in app.dart to MultiBlocProvider,
    added RootCubit provider.

--- lib/app.dart
+++ lib/app.dart
@@ -5,31 +5,44 @@ import 'package:random_quote_app/domain/repositories/quote_repository.dart';
 import 'package:random_quote_app/features/home/cubit/home_cubit.dart';
 import 'package:random_quote_app/features/home/pages/home_page.dart';
 import 'package:random_quote_app/features/about/about_page/about_page.dart';
+import 'package:random_quote_app/features/root/cubit/root_cubit.dart';
+import 'package:random_quote_app/features/settings/settings_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HomeCubit(
-        ImageRepository(),
-        QuoteRepository(),
-      )..start(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<HomeCubit>(
+          create: (context) => HomeCubit(
+            ImageRepository(),
+            QuoteRepository(),
+          )..start(),
+        ),
+        BlocProvider<RootCubit>(
+          create: (context) => RootCubit(),
+        ),
+      ],
       child: BlocBuilder<HomeCubit, HomeState>(
-        builder: (context, state) {
-          return MaterialApp(
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor:
-               state.textColor ?? 
-               Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            routes: {
-              '/': (context) => const HomePage(title: 'Random Quote App'),
-              '/about': (context) => const AboutPage(title: 'About page'),
+        builder: (context, homeState) {
+          return BlocBuilder<RootCubit, RootState>(
+            builder: (context, rootState) {
+              return MaterialApp(
+                theme: ThemeData(
+                  colorSchemeSeed: homeState.textColor ?? Colors.deepPurple,
+                  brightness: rootState.isThemeBright ? Brightness.light : Brightness.dark,
+                  useMaterial3: true,
+                ),
+                routes: {
+                  '/': (context) => const HomePage(title: 'Random Quote App'),
+                  '/about': (context) => const AboutPage(title: 'About page'),
+                  '/settings': (context) => const SettingsPage(title: 'Settings'),
+                },
+                initialRoute: '/',
+              );
             },
-            initialRoute: '/',
           );
         },
       ),
