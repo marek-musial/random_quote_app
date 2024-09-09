6785ea106a4ccdeae0b16faf653118cd39442d5e
MarekMusial <marekmusial07@gmail.com>
2024-09-09 15:13:37 +0200

    Add image repository change on request error
    Added a try catch block that changes chosen image repository on error.

--- lib/domain/repositories/image_repository.dart
+++ lib/domain/repositories/image_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:developer' as dev;
 
 import 'package:random_quote_app/data/remote_data_sources/data_source.dart';
 import 'package:random_quote_app/data/remote_data_sources/image_remote_data_sources/cataas_image_remote_data_source.dart';
@@ -16,8 +17,20 @@ class ImageRepository {
   ImageRepository();
 
   Future<ImageModel?> getImageModel() async {
-    final randomDataSource = imageDataSources[Random().nextInt(imageDataSources.length)];
-
-    return await randomDataSource.getImageData();
+    final int chosenIndex = Random().nextInt(imageDataSources.length);
+    try {
+      final randomDataSource = imageDataSources[chosenIndex];
+      return await randomDataSource.getImageData();
+    } catch (e) {
+      dev.log('$e');
+      int otherIndex;
+      if (chosenIndex < imageDataSources.length - 1) {
+        otherIndex = chosenIndex + 1;
+      } else {
+        otherIndex = chosenIndex - 1;
+      }
+      final randomDataSource = imageDataSources[otherIndex];
+      return await randomDataSource.getImageData();
+    }
   }
 }
