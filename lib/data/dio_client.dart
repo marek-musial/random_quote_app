c1838a219fe114926cca5d11a19cbdbeae6d2add
MarekMusial <marekmusial07@gmail.com>
2024-09-10 21:14:37 +0200

    Make DioClient a Singleton class
    To ensure that on every DioClient() call the same instance is used
    globally across the app.

--- lib/data/dio_client.dart
+++ lib/data/dio_client.dart
@@ -1,17 +1,22 @@
 import 'package:dio/dio.dart';
 
 class DioClient {
+  static final DioClient _instance = DioClient._internal(Dio());
+
   final Dio _dio;
 
-  DioClient(this._dio) {
+  DioClient._internal(this._dio) {
     _dio.options = BaseOptions(
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 3),
     );
   }
 
+  factory DioClient() {
+    return _instance;
+  }
+
   Dio get dio => _dio;
 }
 
-final dio = Dio();
-final DioClient dioClient = DioClient(dio);
+final DioClient dioClient = DioClient();
