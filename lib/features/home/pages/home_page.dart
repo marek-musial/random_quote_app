7415766d513d468f217d27f3b74c8747b60601ac
MarekMusial <marekmusial07@gmail.com>
2024-09-27 19:50:14 +0200

    Create image management dialog widget
    Separated image management dialog widget from home_page code
    for readability.

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -82,35 +82,7 @@ class HomePage extends StatelessWidget {
                                 showDialog(
                                   context: context,
                                   builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text(
-                                        'What do you want to do?',
-                                        style: Theme.of(context).textTheme.titleLarge,
-                                      ),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          style: TextButton.styleFrom(
-                                            textStyle: Theme.of(context).textTheme.labelLarge,
-                                          ),
-                                          child: const Text('Save'),
-                                          onPressed: () {
-                                            final RenderRepaintBoundary boundary =
-                                                widgetToImageKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
-                                            context.read<HomeCubit>().capturePng(boundary);
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                        TextButton(
-                                          style: TextButton.styleFrom(
-                                            textStyle: Theme.of(context).textTheme.labelLarge,
-                                          ),
-                                          child: const Text('Back'),
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                      ],
-                                    );
+                                    return const ImageManagementDialog();
                                   },
                                 );
                               },
