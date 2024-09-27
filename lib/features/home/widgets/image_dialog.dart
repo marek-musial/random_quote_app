8519d5bda637693304d0c53d20ee8ef70c341cb7
MarekMusial <marekmusial07@gmail.com>
2024-09-27 19:50:14 +0200

    Create image management dialog widget
    Separated image management dialog widget from home_page code
    for readability.

---
+++ lib/features/home/widgets/image_dialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:random_quote_app/features/home/cubit/home_cubit.dart';
+import 'package:random_quote_app/features/home/pages/home_page.dart';
+
+class ImageManagementDialog extends StatelessWidget {
+  const ImageManagementDialog({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        'What do you want to do?',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge,
+          ),
+          child: const Text('Save'),
+          onPressed: () {
+            final RenderRepaintBoundary boundary = widgetToImageKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+            context.read<HomeCubit>().capturePng(boundary);
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge,
+          ),
+          child: const Text('Back'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
