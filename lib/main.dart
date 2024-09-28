0dfc25b63d9d75e07cfdbcd70a771e5f35c96ee3
MarekMusial <marekmusial07@gmail.com>
2024-09-28 16:59:27 +0200

    Change image saving to saving to gallery
    Images are now saved to phone gallery using Gal package,
    instead of the app directory.
    Removed unused app directory path initializer from main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,11 @@ import 'package:path_provider/path_provider.dart';
 import 'package:random_quote_app/app.dart';
 import 'package:random_quote_app/core/injection_container.dart';
 
-String? appDirectoryPath;
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
-  final appDirectory = await getApplicationDocumentsDirectory();
-  appDirectoryPath = appDirectory.path;
   configureDependencies();
   runApp(const MyApp());
 }
