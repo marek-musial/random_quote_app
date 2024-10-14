f40197b22a895c1f6af9ef1414f03f6ee7424299
MarekMusial <marekmusial07@gmail.com>
2024-10-14 18:41:46 +0200

    Implement Logger class test

---
+++ test/core/logger_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:random_quote_app/core/logger.dart';
+
+class MockLoggingMethod extends Mock {
+  void call(String message);
+}
+
+void main() {
+  final logger = Logger();
+  group('Logger', () {
+    test('logs message using the provided logging method', () {
+      String message = 'message';
+      MockLoggingMethod mockLoggingMethod = MockLoggingMethod();
+      when(
+        () => mockLoggingMethod(any()),
+      ).thenAnswer(
+        (_) {},
+      );
+      logger.loggingMethod = mockLoggingMethod.call;
+
+      logger.log(message);
+
+      verify(
+        () => mockLoggingMethod.call(message),
+      ).called(1);
+    });
+  });
+}
