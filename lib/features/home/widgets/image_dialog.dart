82f491d968c34183e68ede7c4e3387d09339701f
MarekMusial <marekmusial07@gmail.com>
2024-10-19 20:22:50 +0200

    Update image dialog with share button
    Added the button with sharePng method to the action list.
    Converted AlertDialog to SimpleDialog.
    Wrapped first two buttons with a Row to separate them from the
    'back' button.
    Wrapped the 'back' button in Center to reduce its area.

--- lib/features/home/widgets/image_dialog.dart
+++ lib/features/home/widgets/image_dialog.dart
@@ -11,34 +11,53 @@ class ImageManagementDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
+    return SimpleDialog(
       title: Text(
         'What do you want to do?',
         style: Theme.of(context).textTheme.titleLarge,
+        textAlign: TextAlign.center,
       ),
-      actionsAlignment: MainAxisAlignment.center,
-      actions: <Widget>[
-        TextButton.icon(
-          style: TextButton.styleFrom(
-            textStyle: Theme.of(context).textTheme.labelLarge,
-          ),
-          icon: const Icon(Icons.save_alt),
-          label: const Text('Save'),
-          onPressed: () {
-            final RenderRepaintBoundary boundary = widgetToImageKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
-            context.read<HomeCubit>().capturePng(boundary);
-            Navigator.of(context).pop();
-          },
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              icon: const Icon(Icons.save_alt),
+              label: const Text('Save'),
+              onPressed: () {
+                final RenderRepaintBoundary boundary = widgetToImageKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+                context.read<HomeCubit>().capturePng(boundary);
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              icon: const Icon(Icons.share),
+              label: const Text('Share'),
+              onPressed: () {
+                final RenderRepaintBoundary boundary = widgetToImageKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+                context.read<HomeCubit>().sharePng(boundary);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
-        TextButton.icon(
-          style: TextButton.styleFrom(
-            textStyle: Theme.of(context).textTheme.labelLarge,
+        Center(
+          child: TextButton.icon(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            icon: const Icon(Icons.arrow_back),
+            label: const Text('Back'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
           ),
-          icon: const Icon(Icons.arrow_back),
-          label: const Text('Back'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
         ),
       ],
     );
