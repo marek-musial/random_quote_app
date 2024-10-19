7d85a884c9ff955eba3113f4a9b8b7ed72f40ef8
MarekMusial <marekmusial07@gmail.com>
2024-10-19 19:30:45 +0200

    Initialize temp directory on launch

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:random_quote_app/app.dart';
+import 'package:random_quote_app/core/directories.dart';
 import 'package:random_quote_app/core/injection_container.dart';
 
 void main() async {
@@ -9,6 +10,7 @@ void main() async {
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
+  initializeTempDirectory();
   configureDependencies();
   runApp(const MyApp());
 }
