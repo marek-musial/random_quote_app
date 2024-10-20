45474892e24ee192b4641761cbfff363dd391b95
MarekMusial <marekmusial07@gmail.com>
2024-10-20 17:00:48 +0200

    Fix possible error loop in rerolling data sources
    When the error occurs and the choosen data source is the last one
    from the list, instead of taking the next to last one, takes the first
    one in the list.
    This will prevent an error loop occuring if the last two sources throw
    an error as the default reroll logic is to take the next one in the list
    after an error is thrown.

--- lib/domain/repositories/image_repository.dart
+++ lib/domain/repositories/image_repository.dart
@@ -30,7 +30,7 @@ class ImageRepository {
       if (chosenIndex < dataSources.length - 1) {
         otherIndex = chosenIndex + 1;
       } else {
-        otherIndex = chosenIndex - 1;
+        otherIndex = 0;
       }
       final randomDataSource = dataSources[otherIndex];
       return await randomDataSource.getImageData();
