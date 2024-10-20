96539dee0385c797aa11fbdbdd56302021e054c9
MarekMusial <marekmusial07@gmail.com>
2024-10-20 09:32:33 +0200

    Add logging

--- lib/core/directories.dart
+++ lib/core/directories.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:random_quote_app/core/logger.dart';
 
 String? tempDirectoryPath;
+final Logger _directoryLogger = Logger();
 
 void initializeTempDirectory() async {
   final Directory tempDirectory = await getTemporaryDirectory();
   tempDirectoryPath = tempDirectory.path;
+  _directoryLogger.log('Temp directory initialized');
 }
