c6c7d8dffe91ecfa2abb7eab7ec22011b5a5b97f
MarekMusial <marekmusial07@gmail.com>
2024-10-22 10:37:48 +0200

    Remove unnecessary import
    Removed package flutter/rendering as it is included in material package.

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:random_quote_app/core/enums.dart';
 import 'package:random_quote_app/core/theme/constraints.dart';
