43d1234138cf2be5385ba9e064bcb23f42ca00d2
MarekMusial <marekmusial07@gmail.com>
2024-10-28 23:28:36 +0100

    Switch from Quotable API to Quoteslate
    Due to Quotable API being down for extended time, switched to a similar
    API inspired by the predecessor.

--- lib/data/remote_data_sources/quote_remote_data_sources/quoteslate_quote_remote_data_source.freezed.dart
+++ lib/data/remote_data_sources/quote_remote_data_sources/quoteslate_quote_remote_data_source.freezed.dart
@@ -3,7 +3,7 @@
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
-part of 'quotable_quote_remote_data_source.dart';
+part of 'quoteslate_quote_remote_data_source.dart';
 
 // **************************************************************************
 // FreezedGenerator
@@ -14,56 +14,56 @@ T _$identity<T>(T value) => value;
 final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
-QuotableResponse _$QuotableResponseFromJson(Map<String, dynamic> json) {
-  return _QuotableResponse.fromJson(json);
+QuoteslateResponse _$QuoteslateResponseFromJson(Map<String, dynamic> json) {
+  return _QuoteslateResponse.fromJson(json);
 }
 
 /// @nodoc
-mixin _$QuotableResponse {
-  String get content => throw _privateConstructorUsedError;
+mixin _$QuoteslateResponse {
+  String get quote => throw _privateConstructorUsedError;
   String? get author => throw _privateConstructorUsedError;
 
-  /// Serializes this QuotableResponse to a JSON map.
+  /// Serializes this QuoteslateResponse to a JSON map.
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
 
-  /// Create a copy of QuotableResponse
+  /// Create a copy of QuoteslateResponse
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $QuotableResponseCopyWith<QuotableResponse> get copyWith =>
+  $QuoteslateResponseCopyWith<QuoteslateResponse> get copyWith =>
       throw _privateConstructorUsedError;
 }
 
 /// @nodoc
-abstract class $QuotableResponseCopyWith<$Res> {
-  factory $QuotableResponseCopyWith(
-          QuotableResponse value, $Res Function(QuotableResponse) then) =
-      _$QuotableResponseCopyWithImpl<$Res, QuotableResponse>;
+abstract class $QuoteslateResponseCopyWith<$Res> {
+  factory $QuoteslateResponseCopyWith(
+          QuoteslateResponse value, $Res Function(QuoteslateResponse) then) =
+      _$QuoteslateResponseCopyWithImpl<$Res, QuoteslateResponse>;
   @useResult
-  $Res call({String content, String? author});
+  $Res call({String quote, String? author});
 }
 
 /// @nodoc
-class _$QuotableResponseCopyWithImpl<$Res, $Val extends QuotableResponse>
-    implements $QuotableResponseCopyWith<$Res> {
-  _$QuotableResponseCopyWithImpl(this._value, this._then);
+class _$QuoteslateResponseCopyWithImpl<$Res, $Val extends QuoteslateResponse>
+    implements $QuoteslateResponseCopyWith<$Res> {
+  _$QuoteslateResponseCopyWithImpl(this._value, this._then);
 
   // ignore: unused_field
   final $Val _value;
   // ignore: unused_field
   final $Res Function($Val) _then;
 
-  /// Create a copy of QuotableResponse
+  /// Create a copy of QuoteslateResponse
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
-    Object? content = null,
+    Object? quote = null,
     Object? author = freezed,
   }) {
     return _then(_value.copyWith(
-      content: null == content
-          ? _value.content
-          : content // ignore: cast_nullable_to_non_nullable
+      quote: null == quote
+          ? _value.quote
+          : quote // ignore: cast_nullable_to_non_nullable
               as String,
       author: freezed == author
           ? _value.author
@@ -74,36 +74,36 @@ class _$QuotableResponseCopyWithImpl<$Res, $Val extends QuotableResponse>
 }
 
 /// @nodoc
-abstract class _$$QuotableResponseImplCopyWith<$Res>
-    implements $QuotableResponseCopyWith<$Res> {
-  factory _$$QuotableResponseImplCopyWith(_$QuotableResponseImpl value,
-          $Res Function(_$QuotableResponseImpl) then) =
-      __$$QuotableResponseImplCopyWithImpl<$Res>;
+abstract class _$$QuoteslateResponseImplCopyWith<$Res>
+    implements $QuoteslateResponseCopyWith<$Res> {
+  factory _$$QuoteslateResponseImplCopyWith(_$QuoteslateResponseImpl value,
+          $Res Function(_$QuoteslateResponseImpl) then) =
+      __$$QuoteslateResponseImplCopyWithImpl<$Res>;
   @override
   @useResult
-  $Res call({String content, String? author});
+  $Res call({String quote, String? author});
 }
 
 /// @nodoc
-class __$$QuotableResponseImplCopyWithImpl<$Res>
-    extends _$QuotableResponseCopyWithImpl<$Res, _$QuotableResponseImpl>
-    implements _$$QuotableResponseImplCopyWith<$Res> {
-  __$$QuotableResponseImplCopyWithImpl(_$QuotableResponseImpl _value,
-      $Res Function(_$QuotableResponseImpl) _then)
+class __$$QuoteslateResponseImplCopyWithImpl<$Res>
+    extends _$QuoteslateResponseCopyWithImpl<$Res, _$QuoteslateResponseImpl>
+    implements _$$QuoteslateResponseImplCopyWith<$Res> {
+  __$$QuoteslateResponseImplCopyWithImpl(_$QuoteslateResponseImpl _value,
+      $Res Function(_$QuoteslateResponseImpl) _then)
       : super(_value, _then);
 
-  /// Create a copy of QuotableResponse
+  /// Create a copy of QuoteslateResponse
   /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
-    Object? content = null,
+    Object? quote = null,
     Object? author = freezed,
   }) {
-    return _then(_$QuotableResponseImpl(
-      content: null == content
-          ? _value.content
-          : content // ignore: cast_nullable_to_non_nullable
+    return _then(_$QuoteslateResponseImpl(
+      quote: null == quote
+          ? _value.quote
+          : quote // ignore: cast_nullable_to_non_nullable
               as String,
       author: freezed == author
           ? _value.author
@@ -115,69 +115,69 @@ class __$$QuotableResponseImplCopyWithImpl<$Res>
 
 /// @nodoc
 @JsonSerializable()
-class _$QuotableResponseImpl implements _QuotableResponse {
-  _$QuotableResponseImpl({required this.content, required this.author});
+class _$QuoteslateResponseImpl implements _QuoteslateResponse {
+  _$QuoteslateResponseImpl({required this.quote, required this.author});
 
-  factory _$QuotableResponseImpl.fromJson(Map<String, dynamic> json) =>
-      _$$QuotableResponseImplFromJson(json);
+  factory _$QuoteslateResponseImpl.fromJson(Map<String, dynamic> json) =>
+      _$$QuoteslateResponseImplFromJson(json);
 
   @override
-  final String content;
+  final String quote;
   @override
   final String? author;
 
   @override
   String toString() {
-    return 'QuotableResponse(content: $content, author: $author)';
+    return 'QuoteslateResponse(quote: $quote, author: $author)';
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$QuotableResponseImpl &&
-            (identical(other.content, content) || other.content == content) &&
+            other is _$QuoteslateResponseImpl &&
+            (identical(other.quote, quote) || other.quote == quote) &&
             (identical(other.author, author) || other.author == author));
   }
 
   @JsonKey(includeFromJson: false, includeToJson: false)
   @override
-  int get hashCode => Object.hash(runtimeType, content, author);
+  int get hashCode => Object.hash(runtimeType, quote, author);
 
-  /// Create a copy of QuotableResponse
+  /// Create a copy of QuoteslateResponse
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
   @override
   @pragma('vm:prefer-inline')
-  _$$QuotableResponseImplCopyWith<_$QuotableResponseImpl> get copyWith =>
-      __$$QuotableResponseImplCopyWithImpl<_$QuotableResponseImpl>(
+  _$$QuoteslateResponseImplCopyWith<_$QuoteslateResponseImpl> get copyWith =>
+      __$$QuoteslateResponseImplCopyWithImpl<_$QuoteslateResponseImpl>(
           this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
-    return _$$QuotableResponseImplToJson(
+    return _$$QuoteslateResponseImplToJson(
       this,
     );
   }
 }
 
-abstract class _QuotableResponse implements QuotableResponse {
-  factory _QuotableResponse(
-      {required final String content,
-      required final String? author}) = _$QuotableResponseImpl;
+abstract class _QuoteslateResponse implements QuoteslateResponse {
+  factory _QuoteslateResponse(
+      {required final String quote,
+      required final String? author}) = _$QuoteslateResponseImpl;
 
-  factory _QuotableResponse.fromJson(Map<String, dynamic> json) =
-      _$QuotableResponseImpl.fromJson;
+  factory _QuoteslateResponse.fromJson(Map<String, dynamic> json) =
+      _$QuoteslateResponseImpl.fromJson;
 
   @override
-  String get content;
+  String get quote;
   @override
   String? get author;
 
-  /// Create a copy of QuotableResponse
+  /// Create a copy of QuoteslateResponse
   /// with the given fields replaced by the non-null parameter values.
   @override
   @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$QuotableResponseImplCopyWith<_$QuotableResponseImpl> get copyWith =>
+  _$$QuoteslateResponseImplCopyWith<_$QuoteslateResponseImpl> get copyWith =>
       throw _privateConstructorUsedError;
 }
