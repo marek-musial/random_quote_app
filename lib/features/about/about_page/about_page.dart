b496ef1894b1a7e51a8e249350f197b6f8519128
MarekMusial <marekmusial07@gmail.com>
2024-11-06 18:47:50 +0100

    Fix AppBar title text color to theme color

--- lib/features/about/about_page/about_page.dart
+++ lib/features/about/about_page/about_page.dart
@@ -33,7 +33,15 @@ class AboutPage extends StatelessWidget {
         appBar: MediaQuery.of(context).orientation == Orientation.portrait
             ? AppBar(
                 backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                title: Text(title),
+                title: Text(
+                  title,
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                ),
+                iconTheme: IconThemeData(
+                  color: Theme.of(context).colorScheme.onPrimaryContainer,
+                ),
               )
             : null,
         drawer: const AppBarDrawer(index: 1),
