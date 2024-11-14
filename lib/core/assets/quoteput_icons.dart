2becf76dc0b0dbba7e399f5cd613d2791126cbb7
MarekMusial <marekmusial07@gmail.com>
2024-11-14 18:11:48 +0100

    Add custom Quoteput icon font

---
+++ lib/core/assets/quoteput_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Quoteput {
+  Quoteput._();
+
+  static const _kFontFam = 'Quoteput';
+  static const String? _kFontPkg = null;
+
+  static const IconData quoteput = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
