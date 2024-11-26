e6c4b2c91ba231f655177028af05dafb7a27e633
MarekMusial <marekmusial07@gmail.com>
2024-11-26 17:48:52 +0100

    Import and configure firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:random_quote_app/app.dart';
 import 'package:random_quote_app/core/directories.dart';
 import 'package:random_quote_app/core/injection_container.dart';
+import 'package:random_quote_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  firebase_core
   gal
   share_plus
   url_launcher_windows
