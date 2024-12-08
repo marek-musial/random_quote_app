3f9f9c994fbcd58e1a62eb35c47266ba106e19fd
MarekMusial <marekmusial07@gmail.com>
2024-12-08 21:57:18 +0100

    Add emitPreviousState on navigating to home page
    The state reversal fixes a bug when navigation interrupts
    the image loading process.

--- lib/features/widgets/navigation_drawer.dart
+++ lib/features/widgets/navigation_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:random_quote_app/core/logger.dart';
 import 'package:random_quote_app/core/screen_sizes.dart';
+import 'package:random_quote_app/features/home/cubit/home_cubit.dart';
 
 class AppBarDrawer extends StatelessWidget {
   const AppBarDrawer({
@@ -27,91 +30,100 @@ class AppBarDrawer extends StatelessWidget {
         backgroundColor: Colors.transparent,
         width: screenWidth / 5,
         shape: Border.all(style: BorderStyle.none),
-        child: NavigationRail(
-          backgroundColor: Colors.transparent,
-          onDestinationSelected: (int index) {
-            Navigator.pushReplacementNamed(
-              context,
-              switch (index) {
-                0 => '/',
-                1 => '/about',
-                2 => '/sources',
-                3 => '/settings',
-                int() => '/',
+        child:
+        BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            return NavigationRail(
+              backgroundColor: Colors.transparent,
+              onDestinationSelected: (int index) {
+                globalLogger.log('Page changed to: $index');
+                if (index == 0) {
+                  context.read<HomeCubit>().emitPreviousState();
+                }
+                Navigator.pushReplacementNamed(
+                  context,
+                  switch (index) {
+                    0 => '/',
+                    1 => '/about',
+                    2 => '/sources',
+                    3 => '/settings',
+                    int() => '/',
+                  },
+                );
               },
+              selectedIndex: index ?? 0,
+              labelType: NavigationRailLabelType.all,
+              leading: MediaQuery.of(context).orientation == Orientation.portrait
+                  ? IconButton(
+                      icon: Icon(
+                        Icons.close,
+                        color: textColor,
+                      ),
+                      onPressed: Navigator.canPop(context) ? Navigator.of(context).pop : null,
+                    )
+                  : SizedBox.square(
+                      dimension: screenWidth / 12,
+                      child: Icon(
+                        Icons.menu,
+                        color: textColor,
+                      ),
+                    ),
+              destinations: <NavigationRailDestination>[
+                NavigationRailDestination(
+                  icon: Icon(
+                    Icons.home,
+                    color: textColor,
+                  ),
+                  label: Text(
+                    'Home',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  disabled: index == 0,
+                ),
+                NavigationRailDestination(
+                  icon: Icon(
+                    Icons.info,
+                    color: textColor,
+                  ),
+                  label: Text(
+                    'About',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  disabled: index == 1,
+                ),
+                NavigationRailDestination(
+                  icon: Icon(
+                    Icons.source,
+                    color: textColor,
+                  ),
+                  label: Text(
+                    'Sources',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  disabled: index == 2,
+                ),
+                NavigationRailDestination(
+                  icon: Icon(
+                    Icons.settings,
+                    color: textColor,
+                  ),
+                  label: Text(
+                    'Settings',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  disabled: index == 3,
+                ),
+              ],
             );
           },
-          selectedIndex: index ?? 0,
-          labelType: NavigationRailLabelType.all,
-          leading: MediaQuery.of(context).orientation == Orientation.portrait
-              ? IconButton(
-                  icon: Icon(
-                    Icons.close,
-                    color: textColor,
-                  ),
-                  onPressed: Navigator.canPop(context) ? Navigator.of(context).pop : null,
-                )
-              : SizedBox.square(
-                  dimension: screenWidth / 12,
-                  child: Icon(
-                    Icons.menu,
-                    color: textColor,
-                  ),
-                ),
-          destinations: <NavigationRailDestination>[
-            NavigationRailDestination(
-              icon: Icon(
-                Icons.home,
-                color: textColor,
-              ),
-              label: Text(
-                'Home',
-                style: TextStyle(
-                  color: textColor,
-                ),
-              ),
-              disabled: index == 0,
-            ),
-            NavigationRailDestination(
-              icon: Icon(
-                Icons.info,
-                color: textColor,
-              ),
-              label: Text(
-                'About',
-                style: TextStyle(
-                  color: textColor,
-                ),
-              ),
-              disabled: index == 1,
-            ),
-            NavigationRailDestination(
-              icon: Icon(
-                Icons.source,
-                color: textColor,
-              ),
-              label: Text(
-                'Sources',
-                style: TextStyle(
-                  color: textColor,
-                ),
-              ),
-              disabled: index == 2,
-            ),
-            NavigationRailDestination(
-              icon: Icon(
-                Icons.settings,
-                color: textColor,
-              ),
-              label: Text(
-                'Settings',
-                style: TextStyle(
-                  color: textColor,
-                ),
-              ),
-              disabled: index == 3,
-            ),
-          ],
         ),
       ),
     );
