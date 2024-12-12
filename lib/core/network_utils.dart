21ddaa89cb8eaf88dd983ba472ff48c3511d0d34
MarekMusial <marekmusial07@gmail.com>
2024-12-12 19:36:51 +0100

    Refactor code, add empty list error handling
    Added injectable Connectivity instance for reliable testing
    and code futureproofing.
    Added error handling if the ConnectivityResults list
    returns empty somehow.

--- lib/core/network_utils.dart
+++ lib/core/network_utils.dart
@@ -2,10 +2,15 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:random_quote_app/core/logger.dart';
 
 class NetworkUtils {
+  static Connectivity connectivity = Connectivity();
+
   static Future<bool> checkConnectivity() async {
     try {
-      final connectivityResult = await Connectivity().checkConnectivity();
-      final isConnected = !connectivityResult.contains(ConnectivityResult.none);
+      final connectivityResults = await connectivity.checkConnectivity();
+
+      final isConnected = connectivityResults.isNotEmpty && //R
+          !connectivityResults.contains(ConnectivityResult.none);
+
       globalLogger.log('Connection status: $isConnected');
       return isConnected;
     } catch (e) {
