7aef1e36e2d25892cea4ebddf0c83811810262fb
MarekMusial <marekmusial07@gmail.com>
2025-01-28 16:19:06 +0100

    Initialize and implement NavigationDrawerCubit
    Initialized NavigationDrawerCubit with navigationIndex
    and isFeedbackMode.
    Added NavigationDrawerCubit Provider to app.dart MultiBlocProvider.
    Generated freezed cubit and injection container config.
    Moved logger from NavigationDrawer widget to NavigationDrawerCubit.
    Initialized feedback mode button.

---
+++ lib/features/navigation/cubit/navigation_drawer_cubit.freezed.dart
@@ -0,0 +1,170 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'navigation_drawer_cubit.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$NavigationDrawerState {
+  int get navigationIndex => throw _privateConstructorUsedError;
+  bool get isFeedbackMode => throw _privateConstructorUsedError;
+
+  /// Create a copy of NavigationDrawerState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $NavigationDrawerStateCopyWith<NavigationDrawerState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NavigationDrawerStateCopyWith<$Res> {
+  factory $NavigationDrawerStateCopyWith(NavigationDrawerState value,
+          $Res Function(NavigationDrawerState) then) =
+      _$NavigationDrawerStateCopyWithImpl<$Res, NavigationDrawerState>;
+  @useResult
+  $Res call({int navigationIndex, bool isFeedbackMode});
+}
+
+/// @nodoc
+class _$NavigationDrawerStateCopyWithImpl<$Res,
+        $Val extends NavigationDrawerState>
+    implements $NavigationDrawerStateCopyWith<$Res> {
+  _$NavigationDrawerStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of NavigationDrawerState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? navigationIndex = null,
+    Object? isFeedbackMode = null,
+  }) {
+    return _then(_value.copyWith(
+      navigationIndex: null == navigationIndex
+          ? _value.navigationIndex
+          : navigationIndex // ignore: cast_nullable_to_non_nullable
+              as int,
+      isFeedbackMode: null == isFeedbackMode
+          ? _value.isFeedbackMode
+          : isFeedbackMode // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$NavigationDrawerStateImplCopyWith<$Res>
+    implements $NavigationDrawerStateCopyWith<$Res> {
+  factory _$$NavigationDrawerStateImplCopyWith(
+          _$NavigationDrawerStateImpl value,
+          $Res Function(_$NavigationDrawerStateImpl) then) =
+      __$$NavigationDrawerStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int navigationIndex, bool isFeedbackMode});
+}
+
+/// @nodoc
+class __$$NavigationDrawerStateImplCopyWithImpl<$Res>
+    extends _$NavigationDrawerStateCopyWithImpl<$Res,
+        _$NavigationDrawerStateImpl>
+    implements _$$NavigationDrawerStateImplCopyWith<$Res> {
+  __$$NavigationDrawerStateImplCopyWithImpl(_$NavigationDrawerStateImpl _value,
+      $Res Function(_$NavigationDrawerStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of NavigationDrawerState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? navigationIndex = null,
+    Object? isFeedbackMode = null,
+  }) {
+    return _then(_$NavigationDrawerStateImpl(
+      navigationIndex: null == navigationIndex
+          ? _value.navigationIndex
+          : navigationIndex // ignore: cast_nullable_to_non_nullable
+              as int,
+      isFeedbackMode: null == isFeedbackMode
+          ? _value.isFeedbackMode
+          : isFeedbackMode // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$NavigationDrawerStateImpl implements _NavigationDrawerState {
+  const _$NavigationDrawerStateImpl(
+      {this.navigationIndex = 0, this.isFeedbackMode = false});
+
+  @override
+  @JsonKey()
+  final int navigationIndex;
+  @override
+  @JsonKey()
+  final bool isFeedbackMode;
+
+  @override
+  String toString() {
+    return 'NavigationDrawerState(navigationIndex: $navigationIndex, isFeedbackMode: $isFeedbackMode)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$NavigationDrawerStateImpl &&
+            (identical(other.navigationIndex, navigationIndex) ||
+                other.navigationIndex == navigationIndex) &&
+            (identical(other.isFeedbackMode, isFeedbackMode) ||
+                other.isFeedbackMode == isFeedbackMode));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, navigationIndex, isFeedbackMode);
+
+  /// Create a copy of NavigationDrawerState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$NavigationDrawerStateImplCopyWith<_$NavigationDrawerStateImpl>
+      get copyWith => __$$NavigationDrawerStateImplCopyWithImpl<
+          _$NavigationDrawerStateImpl>(this, _$identity);
+}
+
+abstract class _NavigationDrawerState implements NavigationDrawerState {
+  const factory _NavigationDrawerState(
+      {final int navigationIndex,
+      final bool isFeedbackMode}) = _$NavigationDrawerStateImpl;
+
+  @override
+  int get navigationIndex;
+  @override
+  bool get isFeedbackMode;
+
+  /// Create a copy of NavigationDrawerState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$NavigationDrawerStateImplCopyWith<_$NavigationDrawerStateImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
--- lib/features/navigation/widgets/navigation_drawer.dart
+++ lib/features/navigation/widgets/navigation_drawer.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:random_quote_app/core/logger.dart';
 import 'package:random_quote_app/core/screen_sizes.dart';
 import 'package:random_quote_app/features/home/cubit/home_cubit.dart';
+import 'package:random_quote_app/features/navigation/cubit/navigation_drawer_cubit.dart';
 
 class AppBarDrawer extends StatelessWidget {
   const AppBarDrawer({
@@ -30,99 +30,134 @@ class AppBarDrawer extends StatelessWidget {
         backgroundColor: Colors.transparent,
         width: screenWidth / 5,
         shape: Border.all(style: BorderStyle.none),
-        child: BlocBuilder<HomeCubit, HomeState>(
-          builder: (context, state) {
-            return NavigationRail(
-              backgroundColor: Colors.transparent,
-              onDestinationSelected: (int index) {
-                globalLogger.log('Page changed to: $index');
-                if (index == 0) {
-                  context.read<HomeCubit>().emitPreviousState();
-                }
-                Navigator.pushReplacementNamed(
-                  context,
-                  switch (index) {
-                    0 => '/',
-                    1 => '/about',
-                    2 => '/sources',
-                    3 => '/settings',
-                    int() => '/',
+        child: BlocBuilder<NavigationDrawerCubit, NavigationDrawerState>(
+          builder: (context, navigationState) {
+            return BlocBuilder<HomeCubit, HomeState>(
+              builder: (context, homeState) {
+                return NavigationRail(
+                  backgroundColor: Colors.transparent,
+                  onDestinationSelected: (int index) {
+                    context
+                        .read<NavigationDrawerCubit>() //R
+                        .changeNavigationIndex(index);
+                    if (index == 0) {
+                      context.read<HomeCubit>().emitPreviousState();
+                    }
+                    Navigator.pushReplacementNamed(
+                      context,
+                      switch (index) {
+                        0 => '/',
+                        1 => '/about',
+                        2 => '/sources',
+                        3 => '/settings',
+                        int() => '/',
+                      },
+                    );
                   },
+                  selectedIndex: navigationState.navigationIndex,
+                  labelType: NavigationRailLabelType.all,
+                  leading: MediaQuery.of(context).orientation == Orientation.portrait
+                      ? IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            color: textColor,
+                          ),
+                          onPressed: Navigator.canPop(context) //R
+                              ? Navigator.of(context).pop
+                              : null,
+                        )
+                      : SizedBox.square(
+                          dimension: screenWidth / 12,
+                          child: Icon(
+                            Icons.menu,
+                            color: textColor,
+                          ),
+                        ),
+                  destinations: <NavigationRailDestination>[
+                    NavigationRailDestination(
+                      icon: Icon(
+                        Icons.home,
+                        color: textColor,
+                      ),
+                      label: Text(
+                        'Home',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                      disabled: index == 0,
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(
+                        Icons.info,
+                        color: textColor,
+                      ),
+                      label: Text(
+                        'About',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                      disabled: index == 1,
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(
+                        Icons.source,
+                        color: textColor,
+                      ),
+                      label: Text(
+                        'Sources',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                      disabled: index == 2,
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(
+                        Icons.settings,
+                        color: textColor,
+                      ),
+                      label: Text(
+                        'Settings',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                      disabled: index == 3,
+                    ),
+                  ],
+                  trailing: Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          context
+                              .read<NavigationDrawerCubit>() //R
+                              .toggleFeedbackMode();
+                        },
+                        isSelected: navigationState.isFeedbackMode,
+                        icon: Icon(
+                          Icons.bug_report,
+                          color: Theme.of(context).colorScheme.primary.withOpacity(.8),
+                        ),
+                        focusColor: textColor,
+                        selectedIcon: Icon(
+                          Icons.bug_report,
+                          color: textColor,
+                        ),
+                      ),
+                      Text(
+                        'Toggle feedback mode',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: textColor,
+                            ),
+                      ),
+                    ],
+                  ),
                 );
               },
-              selectedIndex: index ?? 0,
-              labelType: NavigationRailLabelType.all,
-              leading: MediaQuery.of(context).orientation == Orientation.portrait
-                  ? IconButton(
-                      icon: Icon(
-                        Icons.close,
-                        color: textColor,
-                      ),
-                      onPressed: Navigator.canPop(context) //R
-                          ? Navigator.of(context).pop
-                          : null,
-                    )
-                  : SizedBox.square(
-                      dimension: screenWidth / 12,
-                      child: Icon(
-                        Icons.menu,
-                        color: textColor,
-                      ),
-                    ),
-              destinations: <NavigationRailDestination>[
-                NavigationRailDestination(
-                  icon: Icon(
-                    Icons.home,
-                    color: textColor,
-                  ),
-                  label: Text(
-                    'Home',
-                    style: TextStyle(
-                      color: textColor,
-                    ),
-                  ),
-                  disabled: index == 0,
-                ),
-                NavigationRailDestination(
-                  icon: Icon(
-                    Icons.info,
-                    color: textColor,
-                  ),
-                  label: Text(
-                    'About',
-                    style: TextStyle(
-                      color: textColor,
-                    ),
-                  ),
-                  disabled: index == 1,
-                ),
-                NavigationRailDestination(
-                  icon: Icon(
-                    Icons.source,
-                    color: textColor,
-                  ),
-                  label: Text(
-                    'Sources',
-                    style: TextStyle(
-                      color: textColor,
-                    ),
-                  ),
-                  disabled: index == 2,
-                ),
-                NavigationRailDestination(
-                  icon: Icon(
-                    Icons.settings,
-                    color: textColor,
-                  ),
-                  label: Text(
-                    'Settings',
-                    style: TextStyle(
-                      color: textColor,
-                    ),
-                  ),
-                  disabled: index == 3,
-                ),
-              ],
             );
           },
         ),
