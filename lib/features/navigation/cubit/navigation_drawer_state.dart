ea45d7beb2ed106e5ca262663acf122027cfd73d
MarekMusial <marekmusial07@gmail.com>
2025-01-29 00:56:28 +0100

    Implement FeedbackService
    Wrapped MyApp in main and main_production with BetterFeedback.
    Removed isFeedbackMode bool value from navigation_drawer_state.
    Removed toggle on NavigationDrawer button as it only activates
    feedback mode on tap.
    Implemented FeedbackService method in navigation_drawer_cubit with
    turnOnFeedbackMode instead of toggleFeedbackMode.
    Changed feedback button label for clarity.

--- lib/features/navigation/cubit/navigation_drawer_state.dart
+++ lib/features/navigation/cubit/navigation_drawer_state.dart
@@ -4,6 +4,5 @@ part of 'navigation_drawer_cubit.dart';
 class NavigationDrawerState with _$NavigationDrawerState {
   const factory NavigationDrawerState({
     @Default(0) int navigationIndex,
-    @Default(false) bool isFeedbackMode,
   }) = _NavigationDrawerState;
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,3 +1,4 @@
+import 'package:feedback/feedback.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -24,5 +25,9 @@ void main() async {
   await initializeTempDirectory();
   cleanDirectory(tempDirectoryPath);
   configureDependencies();
-  runApp(const MyApp());
+  runApp(
+    BetterFeedback(
+      child: const MyApp(),
+    ),
+  );
 }
