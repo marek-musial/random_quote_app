f038a465b828c6f0f0dca049c6345a99ffa65521
MarekMusial <marekmusial07@gmail.com>
2025-02-20 21:31:52 +0100

    Update SourcesPage with DataSource toggling
    Implement dependency injection of data sources.
    Add information tiles at the end of the list, containing tips
    on toggling data sources.
    Made DataSource required in SourceListTile.
    Made SourceListTile ExpansionTile with the blurb and link button.
    Added Switch for toggling DataSource.

--- lib/features/sources/page/sources_page.dart
+++ lib/features/sources/page/sources_page.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:random_quote_app/core/injection_container.dart';
 import 'package:random_quote_app/core/screen_sizes.dart';
 import 'package:random_quote_app/core/theme/list_tile_style.dart' as tile;
 import 'package:random_quote_app/core/theme/widgets/background_icon_widget.dart';
 import 'package:random_quote_app/data/remote_data_sources/data_source.dart';
-import 'package:random_quote_app/domain/repositories/image_repository.dart';
-import 'package:random_quote_app/domain/repositories/quote_repository.dart';
 import 'package:random_quote_app/features/navigation/widgets/navigation_drawer.dart';
+import 'package:random_quote_app/features/sources/providers/data_source_notifier.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-final List<DataSource> dataSources = [
-  ...imageDataSources,
-  ...quoteDataSources,
-];
+final _imageDataSources = getIt<List<ImageDataSource>>();
+final _quoteDataSources = getIt<List<QuoteDataSource>>();
+final _dataSources = getIt<List<DataSource>>();
 
 class SourcesPage extends StatelessWidget {
   const SourcesPage({super.key, required this.title});
@@ -20,7 +20,6 @@ class SourcesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Color textColor = Theme.of(context).colorScheme.onPrimaryContainer;
-    final Color tileColor = Theme.of(context).colorScheme.primaryContainer.withOpacity(.5);
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -60,55 +59,68 @@ class SourcesPage extends StatelessWidget {
                     : const SizedBox.shrink(),
                 Flexible(
                   flex: 1,
-                  child: ListView.separated(
-                    itemCount: dataSources.length + 2,
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    itemBuilder: (BuildContext context, int index) {
-                      if (index == 0) {
-                        return ListTile(
-                          textColor: textColor,
-                          tileColor: tileColor,
-                          shape: tile.border,
-                          contentPadding: tile.padding,
-                          title: const Text(
-                            'Image sources:',
-                            textAlign: TextAlign.center,
-                          ),
-                          subtitle: const Text(
-                            'All images are a subject of their respective apis licenses.',
-                          ),
-                        );
-                      }
-                      if (index == imageDataSources.length + 1) {
-                        return ListTile(
-                          textColor: textColor,
-                          tileColor: tileColor,
-                          shape: tile.border,
-                          contentPadding: tile.padding,
-                          title: const Text(
-                            'Quote sources:',
-                            textAlign: TextAlign.center,
-                          ),
-                          subtitle: const Text(
-                            'All quotes are a subject of their recpective apis licenses.',
-                          ),
-                        );
-                      }
-                      if (index > imageDataSources.length) {
+                  child: ChangeNotifierProvider(
+                    create: (_) => DataSourceNotifier(),
+                    child: ListView.separated(
+                      itemCount: _dataSources.length + 3,
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      itemBuilder: (BuildContext context, int index) {
+                        if (index == 0) {
+                          return ListTile(
+                            textColor: textColor,
+                            contentPadding: tile.padding,
+                            title: const Text(
+                              'Image sources:',
+                              textAlign: TextAlign.center,
+                            ),
+                            subtitle: const Text(
+                              'All images are a subject of their respective apis licenses.',
+                            ),
+                          );
+                        }
+                        if (index == _imageDataSources.length + 1) {
+                          return ListTile(
+                            textColor: textColor,
+                            contentPadding: tile.padding,
+                            title: const Text(
+                              'Quote sources:',
+                              textAlign: TextAlign.center,
+                            ),
+                            subtitle: const Text(
+                              'All quotes are a subject of their recpective apis licenses.',
+                            ),
+                          );
+                        }
+                        if (index == _imageDataSources.length + _quoteDataSources.length + 2) {
+                          return ListTile(
+                            textColor: Theme.of(context).colorScheme.onPrimaryFixedVariant,
+                            contentPadding: tile.padding,
+                            title: const Text(
+                              'At least one of each type of sources must be enabled at a time!',
+                              textAlign: TextAlign.center,
+                            ),
+                            subtitle: const Text(
+                              'Occasionally, some APIs may return invalid responses or be unavailable.\nHaving more sources active helps prevent API connection errors.',
+                              textAlign: TextAlign.center,
+                            ),
+                          );
+                        }
+                        if (index > _imageDataSources.length) {
+                          return SourceListTile(
+                            dataSource: _dataSources[index - 2],
+                          );
+                        }
                         return SourceListTile(
-                          dataSource: dataSources[index - 2],
+                          dataSource: _dataSources[index - 1],
                         );
-                      }
-                      return SourceListTile(
-                        dataSource: dataSources[index - 1],
-                      );
-                    },
-                    separatorBuilder: (
-                      BuildContext context,
-                      int index,
-                    ) =>
-                        SizedBox(
-                      height: screenHeight / 96,
+                      },
+                      separatorBuilder: (
+                        BuildContext context,
+                        int index,
+                      ) =>
+                          SizedBox(
+                        height: screenHeight / 96,
+                      ),
                     ),
                   ),
                 ),
@@ -124,19 +136,19 @@ class SourcesPage extends StatelessWidget {
 class SourceListTile extends StatelessWidget {
   const SourceListTile({
     super.key,
-    this.dataSource,
+    required this.dataSource,
   });
 
-  final DataSource? dataSource;
+  final DataSource dataSource;
 
   Future<void> _launchUrl() async {
-    if (dataSource?.link != null) {
-      final Uri uri = Uri.parse(dataSource!.link!);
+    if (dataSource.link != null) {
+      final Uri uri = Uri.parse(dataSource.link!);
       if (!await launchUrl(
         uri,
         mode: LaunchMode.platformDefault,
       )) {
-        throw Exception('Could not launch ${dataSource?.link}');
+        throw Exception('Could not launch ${dataSource.link}');
       }
     }
   }
@@ -145,30 +157,46 @@ class SourceListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color textColor = Theme.of(context).colorScheme.onPrimaryContainer;
     final Color containerColor = Theme.of(context).colorScheme.primaryContainer.withOpacity(.5);
+
     return Tooltip(
-      message: dataSource?.link,
+      message: dataSource.link,
       triggerMode: TooltipTriggerMode.longPress,
-      child: InkWell(
-        borderRadius: BorderRadius.all(
-          Radius.circular(screenWidth / 24),
+      child: ExpansionTile(
+        textColor: textColor,
+        collapsedTextColor: textColor,
+        backgroundColor: containerColor,
+        collapsedBackgroundColor: containerColor,
+        shape: tile.border,
+        collapsedShape: tile.border,
+        tilePadding: tile.padding,
+        title: Text(
+          dataSource.title,
+          textAlign: TextAlign.start,
         ),
-        child: ListTile(
-          textColor: textColor,
-          tileColor: containerColor,
-          shape: tile.border,
-          contentPadding: tile.padding,
-          title: Text(
-            dataSource?.title ?? '',
-            textAlign: TextAlign.start,
-          ),
-          subtitle: Text(
-            dataSource?.blurb ?? '',
-            textAlign: TextAlign.start,
-          ),
+        controlAffinity: ListTileControlAffinity.leading,
+        trailing: Consumer<DataSourceNotifier>(
+          builder: (context, dataSourceNotifier, child) {
+            return Switch.adaptive(
+                value: dataSource.isEnabled,
+                onChanged: (_) {
+                  dataSourceNotifier.toggleDataSource(dataSource.title);
+                });
+          },
         ),
-        onTap: () {
-          _launchUrl();
-        },
+        childrenPadding: tile.padding,
+        children: [
+          Text(
+            dataSource.blurb ?? '',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: textColor,
+            ),
+          ),
+          TextButton(
+            onPressed: _launchUrl,
+            child: Text('Go to source'),
+          )
+        ],
       ),
     );
   }
