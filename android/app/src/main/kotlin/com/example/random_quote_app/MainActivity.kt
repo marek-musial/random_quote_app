0e3f375420f3c7fc400f3038bc11ee0c69c468e3
MarekMusial <marekmusial07@gmail.com>
2026-02-06 16:16:08 +0100

    Update MainActivity.kt as per guides

--- android/app/src/main/kotlin/com/example/random_quote_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/random_quote_app/MainActivity.kt
@@ -1,6 +1,35 @@
 package com.marekmusial.random_quote_app
 
+// import io.flutter.embedding.android.FlutterActivity
+
+// class MainActivity: FlutterActivity() {
+// }
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import io.flutter.embedding.android.FlutterActivity
+import android.util.Log
+import android.graphics.Color
 
 class MainActivity: FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        showSystemBars()
+    }
+
+    private fun showSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(true)
+            // API 30 and above
+            window.insetsController?.show(WindowInsets.Type.systemBars())
+        } else {
+            // Below API 30
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_VISIBLE
+            )
+        }
+    }
 }
