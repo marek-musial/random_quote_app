278336f80b532c8e7a5825e8e4c0108ca1445065
MarekMusial <marekmusial07@gmail.com>
2026-02-28 23:44:19 +0100

    Add inverseColor Color extension

--- lib/core/extensions.dart
+++ lib/core/extensions.dart
@@ -10,3 +10,22 @@ extension Brightness on Color {
     }
   }
 }
+
+extension Inversion on Color {
+  Color inverseColor() {
+    if (r > 0.882 && g > 0.882 && b > 0.882) {
+      return Colors.black;
+    }
+
+    if (r < 0.235 && g < 0.235 && b < 0.235) {
+      return Colors.white;
+    }
+
+    return Color.from(
+      red: 1 - r,
+      green: 1 - g,
+      blue: 1 - b,
+      alpha: 1,
+    );
+  }
+}
